f4f89f21cb0967891fd8d167cc1fca3011e05460
A Vijai <97889553+A-Vijai@users.noreply.github.com>
2024-05-14 16:21:08 +0530

    Update upload_to_s3.py

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -6,6 +6,6 @@ bucket_name='customer-rds-project'
 
 def upload_to_s3(filename,datestr):
     file_path=f'raw_data/date={datestr}/{filename}'
-    s3_client.upload(f'tmp/{filename}',bucket_name,file_path)
+    s3_client.upload_file(f'tmp/{filename}',bucket_name,file_path)
     print(f"File uploaded to S3 Bucket {bucket_name}/{file_path}")
     return
